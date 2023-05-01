5ea4d550dd7ed2ce25da253f0ba96e3c4c92f08b
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-05-01 10:26:37 +0000

    Add apps about, contacts and connect restaurant home page

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def restaurant_home_view(request):
+    """View returning the index page"""
+
+    context = {}
+
+    return render(request, 'restaurant/index.html', context)
