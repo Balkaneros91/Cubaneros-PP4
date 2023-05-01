e0f79cf1abbcdd079afee964d22dd138e75ba66f
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-05-01 13:40:07 +0000

    Add views in contacts and templating

--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Contact, OpenHours
 
 
 def contacts_view(request):
     """View returning the contacts page"""
+    contacts = Contact.objects.first()
+    open_hours = OpenHours.objects.all()
 
-    context = {}
+    context = {
+        'contacts': contacts,
+        'open_hours': open_hours,
+    }
 
     return render(request, 'contacts/contacts.html', context)
