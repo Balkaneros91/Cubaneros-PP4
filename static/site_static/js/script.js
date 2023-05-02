a436b84b1ac10ff55857ef21a5e2dcf6e2c11110
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-05-02 11:10:03 +0000

    Add js code for idex menu tabs

--- static/site_static/js/script.js
+++ static/site_static/js/script.js
@@ -3,4 +3,17 @@
 setTimeout(function () {
     document.getElementById('msg').classList.add('hide');
   }, 8000);
-  
+
+
+  // Home page Menu tabs
+
+$(document).ready(function () {
+  // Activate the first tab by default
+  $('#pills-tab a:first').tab('show');
+
+  // Activate the selected tab on click
+  $('#pills-tab a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+});
